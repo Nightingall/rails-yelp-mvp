c1369be6c862db5842931716e986ac1f4bff7aff
Harry Nightingall <nightingallh@gmail.com>
2018-11-07 14:58:13 +0000

    basic functioanlity but no reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '07700 900757'
+  },
+  {
+    name:         'Rendezvous',
+    address:      'Exeter, E1 6PQ',
+    category:     'italian',
+    phone_number: '+44 7700 900954'
+  },
+  {
+    name:         'Rustica',
+    address:      'Edinburgh,  EH17 6PE',
+    category:     'italian',
+    phone_number: '+44 7700 900173'
+  },
+  {
+    name:         'Zizzi',
+    address:      'Putney, SW15 2EL',
+    category:     'italian',
+    phone_number: '07700 900173'
+  },
+  {
+    name:         'Kerridge’s Bar & Grill',
+    address:      'Whitechapel, London E1 6PQ',
+    category:     'french',
+    phone_number: '+44 7700 900954'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
